54d353e5c595c862f0bca002593e28c863b7c21e
Bill Cathey <billcathey@hotmail.com>
2022-09-08 13:34:31 -0400

    Add position prop to popup, display name and description

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-// import { useState, useEffect } from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import { Icon } from 'leaflet'
 import * as parkData from './assets/skate-parks.json'
 import './App.css'
 
 function App() {
-  // const [map, setMap] = useState(null)
-  // useEffect(() => {
-  //   if (!map) return
-  //   map.setView([34.74161249883172, 18.6328125], 2)
-  // }, [map])
   return (
     <div className="App">
-      {/* <MapContainer 
-        center={[51.505, -0.09]}
-        zoom={13}
-        style={{height: "100vh"}}
-        ref={setMap}
-      /> */}
       <MapContainer style={{height: '100%', width: '100%'}} center={[45.31535558124887, -75.69539230092572]} zoom={9} scrollWheelZoom={false}>
         <TileLayer 
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -29,10 +16,19 @@ function App() {
             position={[
               park.geometry.coordinates[1],
               park.geometry.coordinates[0]
-            ]}>
-              <Popup>
-                {park.properties.NAME}
-              </Popup>
+            ]}
+          >
+            <Popup 
+              position={[
+                park.geometry.coordinates[1],
+                park.geometry.coordinates[0]
+              ]}
+            >
+              <div>
+                <h2>{park.properties.NAME}</h2>
+                <p>{park.properties.DESCRIPTIO}</p>
+              </div>
+            </Popup>
           </Marker>
         ))}
       </MapContainer>
