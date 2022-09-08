e543c76d55be19f48391b1045519ff1def3b60a5
Bill Cathey <billcathey@hotmail.com>
2022-09-08 00:05:26 -0400

    Initial map setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+// import { useState, useEffect } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Icon } from 'leaflet'
+import * as parkData from './assets/skate-parks.json'
 import './App.css'
 
 function App() {
-
+  // const [map, setMap] = useState(null)
+  // useEffect(() => {
+  //   if (!map) return
+  //   map.setView([34.74161249883172, 18.6328125], 2)
+  // }, [map])
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      {/* <MapContainer 
+        center={[51.505, -0.09]}
+        zoom={13}
+        style={{height: "100vh"}}
+        ref={setMap}
+      /> */}
+      <MapContainer style={{height: '100%', width: '100%'}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer 
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
